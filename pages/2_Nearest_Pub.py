5c7a63498aefa7ef108fc95a3023d7fdb8b2efdf
Shubham Yadav <112643891+ZealousGuy@users.noreply.github.com>
2023-04-23 05:21:38 +0530

    Add files via upload

--- pages/2_Nearest_Pub.py
+++ pages/2_Nearest_Pub.py
@@ -3,24 +3,26 @@ import pandas as pd
 import numpy as np
 import os
 
-#Page Header
+# Title
 st.header("Find Your Local Pub🍸")
 
 
-#Load data
 ABS = os.path.dirname(os.path.abspath(__file__))
 FILE_DIR = os.path.join(ABS,os.pardir) #Parent dir
 RES_DIR = os.path.join(FILE_DIR, "resources")
 CSV = os.path.join(RES_DIR,"data", "cleaned_open_pubs.csv")
 df = pd.read_csv(CSV)
 
-#Take input -latitude and longitude
+
+
+
 if st.button('Note(Range)'): 
     st.markdown('''
                     Latitude: (49.892485, 60.764969) \n
                     Longitude: (-7.384525, 1.757763) 
                 ''')
     
+# LAT and LONG Input
 c1,c2=st.columns(2)
 with c1:
     lat=st.number_input(label="Latitude", min_value=49.892485, max_value=60.764969)
@@ -29,19 +31,16 @@ with c2:
 
 
 findLoc=np.array((lat,long))
-#Original/available Location
-original_location=np.array([df['latitude'],df['longitude']]).T
+
+og_loc=np.array([df['latitude'],df['longitude']]).T #transposes the 2D array
 
 
 # Now to Find Euclidean dist and save it as a new feature in df
-dist=np.sum((original_location-findLoc)**2, axis=1)
-df['Distance']=dist
+dist=np.sum((og_loc-findLoc)**2, axis=1)
+df['dist']=dist
 
-
-# Slider to know how many nearest pubs to search
-nearest=st.slider(label="How Many Nearest Pub You Want to See",
-                   min_value=1, max_value=50, value=5)
-data=df.sort_values(by='Distance', ascending=True)[:nearest]
+# Now to find nearest 5 PUBS
+data=df.sort_values(by='dist', ascending=True)[:5]
 
 #List of Bar Names
 st.subheader(f":Blue[Pubs Found more details below :point_down:]")
@@ -50,4 +49,4 @@ st.subheader(f":Blue[Pubs Found more details below :point_down:]")
 st.map(data=data, zoom=None, use_container_width=True)
 
 #Name and Address of Nearby Pubs
-st.table(data[['name','address','local_authority']])
+st.table(data[['fsa_id','name','address','dist']])
